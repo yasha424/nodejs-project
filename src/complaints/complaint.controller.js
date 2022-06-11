ab3c68c3811866e236f96adec6a89f4c11d2a877
yasha <jakob20022404@gmail.com>
2022-06-11 23:41:18 +0300

    added validation for complaints

--- src/complaints/complaint.controller.js
+++ src/complaints/complaint.controller.js
@@ -1,13 +1,41 @@
 import { BaseController } from '../common/base.controller.js';
 import { getWeather } from '../services/weather.service.js';
+import { validationMiddleware } from '../common/validation.middleware.js';
+import {
+  getSchema,
+  createSchema,
+  updateSchema,
+  deleteSchema
+} from './schemas/schemas.js';
 
 export class ComplaintController extends BaseController {
   constructor(logger) {
     super(logger);
     this.bindRoutes([
-      { path: '/', method: 'get', func: this.getAll },
-      { path: '/create', method: 'post', func: this.create },
-      { path: '/update', method: 'put', func: this.update }
+      {
+        path: '/',
+        method: 'get',
+        func: this.getAll,
+        middlewares: [validationMiddleware(getSchema)]
+      },
+      {
+        path: '/create',
+        method: 'post',
+        func: this.create,
+        middlewares: [validationMiddleware(createSchema)]
+      },
+      {
+        path: '/update/:id',
+        method: 'put',
+        func: this.update,
+        middlewares: [validationMiddleware(updateSchema)]
+      },
+      {
+        path: '/delete/:id',
+        method: 'delete',
+        func: this.delete,
+        middlewares: [validationMiddleware(deleteSchema)]
+      }
     ]);
   }
 
